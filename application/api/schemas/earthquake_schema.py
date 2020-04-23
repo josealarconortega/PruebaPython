ded72111928f8909e9e43c9c7a89c755da788bf3
Jose Felipe Alarcon <josefelipealarcon@iMac-de-Jose.local>
2020-04-22 21:46:12 -0400

    Se suben cambios

--- application/api/schemas/earthquake_schema.py
+++
@@ -1,9 +0,0 @@
-from application.api.database import ma
-
-
-class earthquakeSchema(ma.ModelSchema):
-    class Meta:
-        fields = ("create_at", "fecha_inicio", "fecha_fin", "magnitud_min", "magnitud_max", "salida")
-
-earthquake_schema = earthquakeSchema()
-earthquakes_schema = earthquakeSchema(many=True)
--- wsgi.py
+++
@@ -1,8 +0,0 @@
-from os import getenv
-
-from application import create_app
-
-app = create_app(getenv('APP_SETTINGS'))
-
-if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8099)
